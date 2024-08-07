4bf11de48d416781453c41e9810309c367cd1568
LindaTa <lindata@maneandmoon.com>
2024-08-07 15:47:35 -0500

    test passed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
+import ipdb
+#     def test_has_title_and_page_count(self):
+#         '''has the title and page_count passed into __init__, and values can be set to new instance.'''
+
+    # def test_requires_int_page_count(self):
+    #     '''prints "page_count must be an integer" if page_count is not an integer.'''
+
+
+    # def test_can_turn_page(self):
+    #     '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
+
+    # FAILED Book in book.py prints "page_count must be an integer" if page_count is not an integer. - AssertionError: assert '' == 'page_count m... an integer\n'
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+book = Book("And Then There Were None", 272)
+book = Book("The World According to Garp", 69)
+
+print(book.title)
+# print(book.page._count)
+
+# ipdb.set_trace()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Initialize the private attribute
+        self.size = size   # Use the property setter to validate the size
+        self.condition = None
+
+# class Shoe:
+#     def __init__(self, brand, size):
+#         self.brand = brand
+#         self.size = size
+#         self._size = None   
+#         self.condition = None
+        
+        #private attribute to store actual size
+
+# FAILED Shoe in shoe.py has the brand and size passed to __init__, and values can be set to new instance. - assert None == 9
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+    
+
+
+
+        
+# FAILED Shoe in shoe.py prints "size must be an integer" if size is not an integer. - AssertionError: assert '' == 'size must be an integer\n'
+
+# FAILED Shoe in shoe.py says that the shoe has been repaired. - AttributeError: 'Shoe' object has no attribute 'cobble'
+
+# FAILED Shoe in shoe.py creates an attribute on the instance called 'condition' and set equal to 'New' after repair. - AttributeError: 'Shoe' object has no attribute 'cobble'
+# ipdb.set_trace()
